31374cdc98d0be3cedaa524a96cd4f56f47b40aa
tiagoslucas <tiago.slucas@gmail.com>
2018-10-17 21:37:20 +0100

    Prototype Design with Scaffolding

---
+++ GestorHorarioG6/GestorHorarioG6/Data/GestorHorarioG6Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestorHorarioG6.Models
+{
+    public class GestorHorarioG6Context : DbContext
+    {
+        public GestorHorarioG6Context (DbContextOptions<GestorHorarioG6Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GestorHorarioG6.Models.Funcionario> Funcionario { get; set; }
+    }
+}
--- GestorHorarioG6/GestorHorarioG6/Startup.cs
+++ GestorHorarioG6/GestorHorarioG6/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using GestorHorarioG6.Models;
 
 namespace GestorHorarioG6
 {
@@ -33,6 +35,9 @@ namespace GestorHorarioG6
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<GestorHorarioG6Context>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("GestorHorarioG6Context")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
