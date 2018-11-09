6de9a5e388b07fbdae9a80c13093e4a63050cb7b
ACcod95 <43669783+ACcod95@users.noreply.github.com>
2018-11-09 12:43:49 +0000

    alterar validacoes do funcionario

--- GestorHorarioG6/GestorHorarioG6/Models/Funcionario.cs
+++ GestorHorarioG6/GestorHorarioG6/Models/Funcionario.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,25 +9,36 @@ namespace GestorHorarioG6.Models
 {
     public class Funcionario
     {
+        [Key]
         public int FuncionarioId { get; set; }
 
-        [Required]
+        [StringLength(60, MinimumLength = 3)]
+        [Required(ErrorMessage = "Por favor insira o Nome")]
         public string Nome { get; set; }
 
-        public string Cargo { get; set; }
+        [Required(ErrorMessage = "Por favor selecione o Cargo")]
+        public string Cargo { get; set; }// alterar para tabela 
 
-        [Required]
+
+        [Required(ErrorMessage = "Porfavor insira a Data de Nascimento ")]
         [DataType(DataType.Date)]
         public DateTime Nascimento { get; set; }
 
-        [Required]
-        public int NIF { get; set; }
-        
-        public int Telefone { get; set; }
+        [Required(ErrorMessage = "Por favor insira o NIF")]
+        [RegularExpression(@"[0-9]{8}")]
+        public string NIF { get; set; }
+
+        [Required(ErrorMessage = "Por favor insira o Número de Telefone")]
+        [RegularExpression(@"(\+[0-9]{3}\s)?[9][0-9]{8}([0-9]{2})?")]
+        public string Telefone { get; set; }
 
         [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"(\w+(\.\w+)*@[a-zA-Z_]+?\.[a-zA-Z]{2,6})", ErrorMessage = "Invalid Email")]
         public string Email { get; set; }
-        
+
         public string Notas { get; set; }
+
+        //[NotMapped] // reformular
+       // public ICollection<Trocas> Trocas { get; set; }
     }
 }
