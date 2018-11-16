c7b8b1d30f78b3c2a32e65d9a3700c78bd077ecf
IamPedroMelo <pedromelop@hotmail.com>
2018-11-16 12:34:26 +0000

    Added Equipamento e Local Model

---
+++ GestorHorarioG6/GestorHorarioG6/Models/Equipamento.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestorHorarioG6.Models
+{
+    public class Equipamento
+    {
+        [Key]
+        public int EquipamentoId{ get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Local { get; set; }
+    }
+}
---
+++ GestorHorarioG6/GestorHorarioG6/Models/Local.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestorHorarioG6.Models
+{
+    public class Local
+    {
+        [Key]
+        public int LocalId { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+    }
+}
