1bd15ef2eb86fcde7e11dceb051bfdcffc76bfad
tiagoslucas <tiago.slucas@gmail.com>
2018-11-23 13:04:18 +0000

    Pesquisa de Requisições por dia

--- GestorHorarioG6/GestorHorarioG6/Models/RequisicoesListViewModel.cs
+++ GestorHorarioG6/GestorHorarioG6/Models/RequisicoesListViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,5 +11,9 @@ namespace GestorHorarioG6.Models
     {
         public IEnumerable<Requisicao> Requisicoes { get; set; }
         public PaginationViewModel PagingInfo { get; set; }
+
+        [DisplayName("HoraDeInicio")]
+        [DataType(DataType.Date)]
+        public DateTime CurrentDay { get; set; }
     }
 }
