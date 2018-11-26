b7583d26a73c487fef6f578acbe86a74e90fd457
IamPedroMelo <pedromelop@hotmail.com>
2018-11-26 15:11:06 +0000

    SearchString por bloco

---
+++ GestorHorarioG6/GestorHorarioG6/Models/EquipamentosViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestorHorarioG6.Models
+{
+    public class EquipamentosViewModel
+    {
+        public IEnumerable<Equipamento> Equipamento { get; set; }
+        public PaginationViewModel PageInfo { get; set; }
+
+        [DisplayName("Nome")]
+        public string CurrentNome { get; set; }
+    }
+}
