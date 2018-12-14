ce72ef0536152e83747d03a5fbcd5b3b2fb46749
IamPedroMelo <pedromelop@hotmail.com>
2018-12-14 11:58:18 +0000

    Criação do controller das regras gerais

---
+++ GestorHorarioG6/GestorHorarioG6/Models/RegrasGerais.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestorHorarioG6.Models
+{
+    public class RegrasGerais
+    {
+        [Key]
+        public int RegraId { get; set; }
+
+        [Required(ErrorMessage = "Por favor insira um nome válido")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "O nome tem de ter entre 3 a 50 caracteres")]
+        public string Nome { get; set; }
+
+        [StringLength(500, ErrorMessage = "O nome tem de ter menos de 50 caracteres")]
+        public string Descricao { get; set; }
+
+        public int Horas { get; set; }
+    }
+}
